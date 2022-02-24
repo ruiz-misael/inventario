d2281dc24b32cd895ec8e0690e116998a1d329ee
Misael <oreb.ruiz@gmail.com>
2022-02-23 22:57:12 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+const routes: Routes = [
+{path:'home', component:HomeComponent},
+{path:'', component:HomeComponent},
+{path:'form', component:FormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
